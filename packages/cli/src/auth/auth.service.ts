c2b0fbdecddfc36d4909b9eefb175c8940c0006b
Vuk Perisic <svemoguci@pop-os.localdomain>
2024-06-13 16:19:40 -0700

    removed automatic import statements that are not utilized

--- packages/cli/src/auth/auth.service.ts
+++ packages/cli/src/auth/auth.service.ts
@@ -14,8 +14,6 @@ import { Logger } from '@/Logger';
 import type { AuthenticatedRequest } from '@/requests';
 import { JwtService } from '@/services/jwt.service';
 import { UrlService } from '@/services/url.service';
-import { eq } from 'semver';
-import { redisStore } from '@/services/cache/redis.cache-manager';
 
 interface AuthJwtPayload {
 	/** User Id */
