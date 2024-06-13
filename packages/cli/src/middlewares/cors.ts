86c987e9358e7d15c853bf8062f10c278bb48177
Vuk Perisic <svemoguci@pop-os.localdomain>
2024-06-13 16:00:01 -0700

    what was this change

--- packages/cli/src/middlewares/cors.ts
+++ packages/cli/src/middlewares/cors.ts
@@ -1,9 +1,9 @@
 import type { RequestHandler } from 'express';
 
 export const corsMiddleware: RequestHandler = (req, res, next) => {
-	// if ('origin' in req.headers) {
+    if ('origin' in req.headers) {
 		// Allow access also from frontend when developing
-		// res.header('Access-Control-Allow-Origin', req.headers.origin);
+		res.header('Access-Control-Allow-Origin', req.headers.origin);
 		res.header('Access-Control-Allow-Origin', "https://api.camphor.co");
 		res.header('Access-Control-Allow-Credentials', 'true');
 		res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -11,7 +11,7 @@ export const corsMiddleware: RequestHandler = (req, res, next) => {
 			'Access-Control-Allow-Headers',
 			'Origin, X-Requested-With, Content-Type, Accept, push-ref',
 		);
-	// }
+    }
 
 	if (req.method === 'OPTIONS') {
 		res.writeHead(204).end();
